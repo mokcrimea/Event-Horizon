e75fb1aeb68df8213c5882011032c64d68e34bcf
Valentyn Khatsaiuk <mok.crimea@gmail.com>
2014-03-07 20:04:21 +0000

    Добавил middleware sendHttpError

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ var http = require('http');
 var path = require('path');
 var log = require('lib/log')(module);
 var config = require('config');
+var HttpError = require('error').HttpError;
 
 var app = express();
 
 // all environments
-app.set('port', config.get('port'));
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'templates'));
 app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
@@ -16,13 +16,30 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
+
+app.use(require('middleware/sendHttpError'));
+
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+app.use(function(err, req, res, next) {
+  if (typeof err == 'number') { // next(404);
+    err = new HttpError(err);
+  }
+
+  if (err instanceof HttpError) {
+    res.sendHttpError(err);
+  } else {
+    if (app.get('env') == 'development') {
+      express.errorHandler()(err, req, res, next);
+    } else {
+      log.error(err);
+      err = new HttpError(500);
+      res.sendHttpError(err);
+    }
+  }
+});
+
 
 http.createServer(app).listen(config.get('port'), function() {
   log.info('Express server listening on port ' + config.get('port'));
