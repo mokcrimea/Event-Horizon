b2d08037f91c0f0604c48ebf869a32eae8072286
Vitaliy Cherkov <MaestroCrimea@yandex.ua>
2014-03-24 23:41:38 +0200

    Реализована функция перелистывания фото в режиме просмотрщика

--- public/flatDesign/script/slideShow.js
+++ public/flatDesign/script/slideShow.js
@@ -1,9 +1,11 @@
 document.body.addEventListener('click', function(event) {
     if (event.target.className.search('gallery-zoom-btn') != -1) openSlideShow(event.target.parentNode);
     if (event.target.className.search('slide-close-btn') != -1) closeSlideShow(event.target.parentNode);
+    if (event.target.className.search('slide-next-btn') != -1) showNextPhoto();
+    if (event.target.className.search('slide-prev-btn') != -1) showPrevPhoto();
 }, true);
 
-
+var inputDataObject = [{"links":{"XXS":{"width":75,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6d_a01a0758_XXS","height":75},"XL":{"width":800,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6d_a01a0758_XL","height":450},"M":{"width":300,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6d_a01a0758_M","height":169},"L":{"width":500,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6d_a01a0758_L","height":281},"XXXS":{"width":50,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6d_a01a0758_XXXS","height":50},"XXXL":{"width":1280,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6d_a01a0758_XXXL","height":720},"S":{"width":150,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6d_a01a0758_S","height":84},"XS":{"width":100,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6d_a01a0758_XS","height":56},"XXL":{"width":1024,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6d_a01a0758_XXL","height":576},"orig":{"width":1920,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6d_a01a0758_orig","bytesize":293390,"height":1080}},"self":"http://api-fotki.yandex.ru/api/users/maestrocrimea/photo/842861/","param":"urn:yandex:fotki:maestrocrimea:photo:842861","_id":"53308122f330b2400706e269","coordinates":[]},{"links":{"XXS":{"width":75,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6f_c1da5ddf_XXS","height":75},"XL":{"width":800,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6f_c1da5ddf_XL","height":450},"M":{"width":300,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6f_c1da5ddf_M","height":169},"L":{"width":500,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6f_c1da5ddf_L","height":281},"XXXS":{"width":50,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6f_c1da5ddf_XXXS","height":50},"XXXL":{"width":1280,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6f_c1da5ddf_XXXL","height":720},"S":{"width":150,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6f_c1da5ddf_S","height":84},"XS":{"width":100,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6f_c1da5ddf_XS","height":56},"XXL":{"width":1024,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6f_c1da5ddf_XXL","height":576},"orig":{"width":1920,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6f_c1da5ddf_orig","bytesize":0,"height":1080}},"self":"http://api-fotki.yandex.ru/api/users/maestrocrimea/photo/842863/","param":"urn:yandex:fotki:maestrocrimea:photo:842863","_id":"53308124f330b2400706e26a","coordinates":[]},{"links":{"XXS":{"width":75,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6e_9857be8c_XXS","height":75},"XL":{"width":800,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6e_9857be8c_XL","height":450},"M":{"width":300,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6e_9857be8c_M","height":169},"L":{"width":500,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6e_9857be8c_L","height":281},"XXXS":{"width":50,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6e_9857be8c_XXXS","height":50},"XXXL":{"width":1280,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6e_9857be8c_XXXL","height":720},"S":{"width":150,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6e_9857be8c_S","height":84},"XS":{"width":100,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6e_9857be8c_XS","height":56},"XXL":{"width":1024,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6e_9857be8c_XXL","height":576},"orig":{"width":1920,"href":"http://img-fotki.yandex.ru/get/9827/231475615.0/0_cdc6e_9857be8c_orig","bytesize":0,"height":1080}},"self":"http://api-fotki.yandex.ru/api/users/maestrocrimea/photo/842862/","param":"urn:yandex:fotki:maestrocrimea:photo:842862","_id":"53308124f330b2400706e26b","coordinates":[]}];
 
 
 function openSlideShow(imgContainerGallery){
@@ -19,7 +21,7 @@ function openSlideShow(imgContainerGallery){
   var mainWindowSlide = document.getElementById('slideShow'),
       backgroundBlock = document.getElementsByClassName('backgroundSpace')[0],
       thisImgSrc = imgContainerGallery.childNodes[5].childNodes[1].src,
-      slideImg = mainWindowSlide.childNodes[3].childNodes[0],
+      slideImg = document.getElementById('slide-img'),
       originalImg = new Image();
       
   mainWindowSlide.style.display = 'block';
@@ -51,7 +53,7 @@ function openSlideShow(imgContainerGallery){
           slideImg.setAttribute('height', '750px');
         }
       }
-      
+
     }
     slideImg.style.opacity = "1";
     return;
@@ -59,12 +61,37 @@ function openSlideShow(imgContainerGallery){
   
 }
 
-
-
-
-
 function closeSlideShow(slideShowWindow){
   slideShowWindow.style.display = "none";
-  slideShowWindow.childNodes[3].firstChild.style.opacity = "0";
+  document.getElementById('slide-img').style.opacity = "0";
   document.getElementsByClassName('backgroundSpace')[0].style.display = "none";
+}
+
+function showNextPhoto(){
+  changePhotoSlide(true);
+}
+
+function showPrevPhoto(){
+  changePhotoSlide(false);
+}
+
+function changePhotoSlide(bool){
+  var arraySrcList=[],
+      nowImg = document.getElementById('slide-img'),
+      positionPhoto;
+
+  for (var i = 0; i < inputDataObject.length; i++){
+    arraySrcList[i] = inputDataObject[i].links.orig.href;
+  }
+
+  positionPhoto = arraySrcList.indexOf('nowImg.src');
+  if (bool){
+    if (positionPhoto != arraySrcList.length - 1){
+    nowImg.src = arraySrcList[positionPhoto + 1];
+    }
+  } else {
+    if (positionPhoto != 0){
+      nowImg.src = arraySrcList[positionPhoto - 1];
+    }
+  }
 }
