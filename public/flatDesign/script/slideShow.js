f21529b649f1f3db00e702a17b3aa14eea98ef2f
Vitaliy Cherkov <MaestroCrimea@yandex.ua>
2014-03-24 20:28:36 +0200

    Фотография при открытии просмотрщика - теперь подстраивается под окно

--- public/flatDesign/script/slideShow.js
+++ public/flatDesign/script/slideShow.js
@@ -3,6 +3,9 @@ document.body.addEventListener('click', function(event) {
     if (event.target.className.search('slide-close-btn') != -1) closeSlideShow(event.target.parentNode);
 }, true);
 
+
+
+
 function openSlideShow(imgContainerGallery){
 
   function getClientWidth(){
@@ -16,7 +19,8 @@ function openSlideShow(imgContainerGallery){
   var mainWindowSlide = document.getElementById('slideShow'),
       backgroundBlock = document.getElementsByClassName('backgroundSpace')[0],
       thisImgSrc = imgContainerGallery.childNodes[5].childNodes[1].src,
-      slideImg = mainWindowSlide.childNodes[3].childNodes[0];
+      slideImg = mainWindowSlide.childNodes[3].childNodes[0],
+      originalImg = new Image();
       
   mainWindowSlide.style.display = 'block';
 
@@ -26,11 +30,41 @@ function openSlideShow(imgContainerGallery){
   backgroundBlock.style.width = getClientWidth() + 'px';
   backgroundBlock.style.height = getClientHeight() + 'px';
 
+  slideImg.removeAttribute('width');
+  slideImg.removeAttribute('height');
   slideImg.src = thisImgSrc;
-  slideImg.setAttribute('width', '1000px');
+  originalImg.src = thisImgSrc;
+  originalImg.onload = function(){
+    var originalHeight = originalImg.height,
+        originalWidth = originalImg.width,
+        differentSize;
+
+    if ((originalHeight > 750) || (originalWidth > 1100)){
+
+      if (originalHeight > originalWidth){
+        slideImg.setAttribute('height', '750px');
+      }
+
+      if (originalWidth > originalHeight){
+        differentSize = 1100 * originalHeight / originalWidth;
+        if (differentSize > 750){
+          slideImg.setAttribute('height', '750px');
+        }
+      }
+      
+    }
+    slideImg.style.opacity = "1";
+    return;
+  }
+  
 }
+
+
+
+
 
 function closeSlideShow(slideShowWindow){
   slideShowWindow.style.display = "none";
+  slideShowWindow.childNodes[3].firstChild.style.opacity = "0";
   document.getElementsByClassName('backgroundSpace')[0].style.display = "none";
 }
