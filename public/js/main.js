a51e30055380bb6964722a9adc38dd8c974c3264
Vitaliy Cherkov <MaestroCrimea@yandex.ua>
2014-03-31 19:05:16 +0400

    Скрипт системы ошибок для страницы 'Загрузить трек'

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,44 @@
-function setAlertMessage(message){
+window.onload = function(){
+  var reTitle = /(^[A-zА-я0-9\s.,_-еЁ]{3,50}$)/,
+      inputForm = document.getElementsByClassName('form-input-load-text')[0],
+      loadIndicator = document.getElementsByClassName('load-file-indicator')[0],
+      alertWindow = document.getElementsByClassName('alert-message')[0],
+      re = new RegExp(), 
+      form = document.upload_track,
+      validExtensions = [".gpx"],
+      arrayOfFiles = form.upload;
 
-  var alertWindow = document.getElementsByClassName('alert-message')[0];
+  document.addEventListener("keyup", function(){
+    if (!reTitle.test(inputForm.value)) {
+      alertWindow.innerHTML = 'Недопустимое имя';
+      alertWindow.style.opacity = '1';
+    } else{
+      alertWindow.style.opacity = '0';
+    }
+  });
 
-  alertWindow.innerHTML = message;
-  alertWindow.style.opacity = '1';
+  document.getElementsByClassName('list-information-gallery')[0].addEventListener("click", function(e){
+    if (!reTitle.test(form.title.value)) {
+      alertWindow.innerHTML = 'Недопустимое имя файла!';
+      return e.preventDefault();
+    }
 
-  document.addEventListener('click', turnAlertMessage);
+    if (form.upload.type == 'file') {
+      for (var i = 0; i < validExtensions.length; i++) {
+        var re = new RegExp(validExtensions[i] + '$', 'i');
+        if (!re.test(arrayOfFiles.value)) {
+          alertWindow.innerHTML = 'Оповещение о неправильном формате файла';
+          alertWindow.style.opacity = '1';
+          return e.preventDefault();
+        }
+      }
+    }
+    loadIndicator.style.display = "block";
+  });
 
-  function turnAlertMessage(){  
-    alertWindow.style.opacity = '0';
-    document.body.removeEventListener('click', turnAlertMessage);
-  }
+  document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 27) {
+      loadIndicator.style.display = "none";
+    }
+  });
 }
