1337ef87bdc26bfc0f950f03e4a0dc8034fa8295
Vitaliy Cherkov <MaestroCrimea@yandex.ua>
2014-03-31 17:24:02 +0400

    Всплывающее окно при неправильно формате входящих данных

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,14 @@
-var setAlertMessage = function(message, elem){
+function setAlertMessage(message){
 
-  var turnAlertMessage = function(){  
-    var alertList = document.body.getElementsByClassName('alert-message');
-    for(var i = 0; i < alertList.length; i++) alertList[i].style.opacity = '0';
+  var alertWindow = document.getElementsByClassName('alert-message')[0];
+
+  alertWindow.innerHTML = message;
+  alertWindow.style.opacity = '1';
+
+  document.addEventListener('click', turnAlertMessage);
+
+  function turnAlertMessage(){  
+    alertWindow.style.opacity = '0';
     document.body.removeEventListener('click', turnAlertMessage);
   }
-
-  var thisElem = document.body.getElementsByClassName('form-group')[elem];
-  thisElem.innerHTML = '<div class="alert-message">' + message + '</div>';
-
-  setTimeout(function(){
-    var alertList = document.body.getElementsByClassName('alert-message');
-    for(var i = 0; i < alertList.length; i++) alertList[i].style.opacity = '1';
-
-    document.body.addEventListener('click', turnAlertMessage);
-
-  }, 10);
 }
-
-setAlertMessage('Проверка', 4);
-setAlertMessage('Проверка 2 3 1 тест ', 3);
